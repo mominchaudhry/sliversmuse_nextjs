2ec7d883451f486d29a422676ae2efb029320a8d
Momin Chaudhry <momin@Momins-MacBook-Pro.local>
2022-03-01 23:03:15 -0500

    updated checkout

--- components/SuccessPage/SuccessPage.js
+++ components/SuccessPage/SuccessPage.js
@@ -12,7 +12,7 @@ function SuccessPage() {
         console.log('momin',  session_id)
         const confirmedOrder = await axios.post("https://sliversmuse.herokuapp.com/api/orders/confirm", {checkout_session: session_id});
         if (confirmedOrder.status === 200) {
-            setOrder(confirmedOrder.data)
+            setOrder(confirmedOrder)
         }
     }
 
@@ -26,14 +26,14 @@ function SuccessPage() {
   return (
     <div className="d-flex flex-column align-items-center justify-content-around">
 
-        {order && <>
+        {order && order.status === 200 && <>
         <div className="text header d-flex justify-content-center myfont">
             ORDER SUCCEEDED!
         </div>
         <div className="d-flex align-items-center logo-section justify-content-center">
             <img loading="lazy" src='/Assets/logo_animated.gif' alt="logo" className="logo" />
             <div className="d-flex justify-content-center flex-column success-section align-self-center">
-            <p className='myfont order-number'>Order #{order.id}</p>
+            <p className='myfont order-number'>Order #{order.data.id}</p>
             <p className='myfont success-message'>your beats will be sent to your email, 
             please check your spam or junk folder if the email is not in your main inbox. 
             If you don&#39;t receive an email within 24 hours, please contact <a
@@ -43,7 +43,7 @@ function SuccessPage() {
             </div>
         </div>
       </>}
-      {!order && <>
+      {order && order.status !== 200 && <>
         <div className="text header d-flex justify-content-center myfont">
             ORDER FAILED!
         </div>
