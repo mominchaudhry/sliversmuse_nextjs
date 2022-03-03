484c4cdff1a39ef642f2b257dc5d6b3f831969c4
Momin Chaudhry <momin@Momins-MacBook-Pro.local>
2022-03-03 17:08:47 -0500

    SEO

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
             <meta charSet="UTF-8"/>
             <meta name="description" content="Buy high quality beats now from sliversmuse"/>
