a28a2dd2ea2a503f5dcd077f154da87246e0e1c6
Momin Chaudhry <momin@Momins-MacBook-Pro.local>
2022-04-06 22:09:20 -0400

    fixed bugs

--- pages/success.js
+++ pages/success.js
@@ -23,7 +23,7 @@ export default function Success() {
         </title>
       </Head>
       <div className="d-flex flex-column justify-content-between main-section">
-        <MyNavbar setLoading={setIsLoading} beats={false}/>
+        <MyNavbar setLoading={setIsLoading} beats={true}/>
         <SuccessPage />
         <div className="d-flex justify-content-center w-100">
           <img loading="lazy" className="godmode" src="/Assets/godmode.png" alt="godmode" />
